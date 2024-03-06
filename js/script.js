1faafb664ff0540ca912ae19e6c52c61afe03157
Jack Shorter <jshorter206854@gmail.com>
2024-03-06 12:28:36 +0000

    created func to loop through book array and display them to the user

--- js/script.js
+++ js/script.js
@@ -8,13 +8,39 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function info(){
-        return '${title} by ${author}, ${pages} pages, ${read}.'
-    };
 }
 
 // func to add book to library list
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+const books = document.querySelector('.main-grid-container');
+
+function displayBooks(bookArray) {
+    for (i=0; i < bookArray.length; i++) {
+        
+        const book = document.createElement('div'); // creates book container
+        book.classList.add('book-card'); // adds book-card styling class to newly created book
+        
+
+        const title = document.createElement('div');
+        const titleText = document.createTextNode(`${bookArray[i].title}`);
+        title.appendChild(titleText);
+
+        const author = document.createElement('div');
+        const authorText = document.createTextNode(`${bookArray[i].author}`);
+        author.appendChild(authorText);
+
+        const pages = document.createElement('div');
+        const pagesText = document.createTextNode(`${bookArray[i].pages} pages`);
+        pages.appendChild(pagesText);
+        
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+
+        books.appendChild(book);
+    }
 }
